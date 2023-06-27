99269e1c1fc192cf9d28f15639ec98445d4cb5cd
IndoorCorgi <indoorcorgi@gmail.com>
2023-06-27 12:32:19 +0900

    バージョン1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cgpmgr',
-    version='1.4',
+    version='1.5',
     description='Controller tool of Raspberry Pi power management board "RPZ-PowerMGR".',
     author='Indoor Corgi',
     author_email='indoorcorgi@gmail.com',
